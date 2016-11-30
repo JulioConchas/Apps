addd65113bb96845cbdfd78ac4d15a2101c9e4a2
JulioConchas <conchasjimenez@gmail.com>
2016-11-29 20:56:51 -0600

    Dinamic main,client,kitchen,waitress fragments added

--- Waitress/app/src/main/java/com/example/jc/waitress/MainActivity.java
+++ Waitress/app/src/main/java/com/example/jc/waitress/MainActivity.java
@@ -3,34 +3,54 @@ package com.example.jc.waitress;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Main.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //have to validate if this terminal is a client terminal
-        SharedPreferences prefe = getSharedPreferences("terminals", Context.MODE_PRIVATE);
+
+        SharedPreferences prefe = getSharedPreferences("terminals", Context.MODE_PRIVATE);          //opteniento tipo de terminal
+
+        /******* Conectando con fracment ********/
+        //Paso 1: Obtener la instancia del administrador de fragmentos
+        FragmentManager fragmentManager = getFragmentManager();
+        //Paso 2: Creat una nueva transacción
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
 
         switch(prefe.getString("Trade_kind","")){
             case "Waitress":
                 System.out.println("**** open waitress's fragment : MainActivity class **** ");
+                waitress waitrFragment = new waitress();
+                transaction.add(R.id.mainLayout, waitrFragment);
                 break;
             case "Kitchen":
                 System.out.println("**** open kitchen's fragment : MainActivity class **** ");
+                kitchen kitFragment = new kitchen();
+                transaction.add(R.id.mainLayout, kitFragment);
                 break;
             case "Client":
                 System.out.println("**** open client's fragment : MainActivity class **** ");
+                client clFragment = new client();
+                transaction.add(R.id.mainLayout, clFragment);
                 break;
             default:
-                System.out.println("****** Trade king in MainActivity class : " + prefe.getString("Trade_kind",""));
+                System.out.println("**** open as default main's fragment : MainActivity class **** ");
+                Main mainFragment = new Main();
+                transaction.add(R.id.mainLayout, mainFragment);
                 break;
         }
+        //4.- Confirmar el cambio
+        transaction.commit();
 
     }
     public void singUp(View view){
@@ -45,5 +65,8 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this,Client_Connection.class);
         startActivity(i);
         finish();
+    }
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
     }
 }
--- Waitress/app/src/main/java/com/example/jc/waitress/terminals.java
+++ Waitress/app/src/main/java/com/example/jc/waitress/terminals.java
@@ -1,5 +1,7 @@
 package com.example.jc.waitress;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -15,19 +17,35 @@ public class terminals extends AppCompatActivity {
         //have to validate if this terminal is a client terminal
         SharedPreferences prefe = getSharedPreferences("terminals", Context.MODE_PRIVATE);
 
+        /******* Conectando con fracment ********/
+        //Paso 1: Obtener la instancia del administrador de fragmentos
+        FragmentManager fragmentManager = getFragmentManager();
+        //Paso 2: Creat una nueva transacción
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
         switch(prefe.getString("Trade_kind","")){
             case "Waitress":
                 System.out.println("**** open waitress's fragment : terminals class **** ");
+                waitress waitrFragment = new waitress();
+                transaction.add(R.id.mainLayout, waitrFragment);
                 break;
             case "Kitchen":
                 System.out.println("**** open kitchen's fragment : terminals class **** ");
+                kitchen kitFragment = new kitchen();
+                transaction.add(R.id.mainLayout, kitFragment);
                 break;
             case "Client":
                 System.out.println("**** open client's fragment : terminals class **** ");
+                client clFragment = new client();
+                transaction.add(R.id.mainLayout, clFragment);
                 break;
             default:
-                System.out.println("****** Trade king in terminal class : " + prefe.getString("Trade_kind",""));
+                System.out.println("**** open as default main's fragment : terminals class **** ");
+                Main mainFragment = new Main();
+                transaction.add(R.id.mainLayout, mainFragment);
                 break;
         }
+        //4.- Confirmar el cambio
+        transaction.commit();
     }
 }
